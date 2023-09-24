14079d5612986cb4cccdc27d38dd5c9525e0bdaf
s.saletti <s.saletti@qzrstudio.com>
2023-09-25 00:35:17 +0200

    render

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ from endpoints import query_bot_endpoint
 app = Flask(__name__)
 CORS(app)
 
-
-
 app.add_url_rule('/query_bot', 'query_bot', query_bot_endpoint, methods=['GET'])
 # app.add_url_rule('/query_bot_stream', 'query_bot_stream', sse_stream_endpoint, methods=['GET'])
 
-
-if __name__ == '__main__':
-	app.run(debug=False)
+# if __name__ == '__main__':
+# 	app.run(debug=False)
