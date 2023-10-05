4ab1a69c977258db1fb74f4c2ce88b1d3d9e1dc0
s.saletti <s.saletti@qzrstudio.com>
2023-10-05 21:30:01 +0200

    push

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, g
 from flask_cors import CORS  
 from endpoints import query_bot_endpoint
 from utilities import initialize_tools  
+from embedding_generator import create_embeddings
 
 # these lines swap the stdlib sqlite3 lib with the pysqlite3 package
 __import__('pysqlite3')
@@ -12,6 +13,7 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 app = Flask(__name__)
 CORS(app)
 
+create_embeddings()
 initialize_tools()
 
 app.add_url_rule('/query_bot', 'query_bot', query_bot_endpoint, methods=['GET'])
--- embedding_generator.py
+++ embedding_generator.py
@@ -13,55 +13,56 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 DEPLOYMENT_ENV = os.environ.get('DEPLOYMENT_ENV', 'DEVELOPMENT')
 
-if DEPLOYMENT_ENV == 'PRODUCTION':
-    base_directory = "/var/data/embeddings/"
-else:
-    base_directory = ".\\embeddings\\"
+def create_embeddings():
+	if DEPLOYMENT_ENV == 'PRODUCTION':
+		base_directory = "/var/data/embeddings/"
+	else:
+		base_directory = ".\\embeddings\\"
 
-splits = []    
-pdfs = [
-	"documents/main.md",
-	"documents/renown.md"
-]
-
-csvs = [
-	"documents/cards.csv"
-]
-
-gitbooks = [
-	'https://docs.echelon.io/'
-]
-
-for document in pdfs:
-	with open(document, "r") as f:
-		markdown_document = f.read()
-
-	headers_to_split_on = [
-		("##", "section"),
-		("###", "topic"),
+	splits = []    
+	pdfs = [
+		"documents/main.md",
+		"documents/renown.md"
 	]
 
-	markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
-	md_header_splits = markdown_splitter.split_text(markdown_document)    
-	splits.extend(md_header_splits)
+	csvs = [
+		"documents/cards.csv"
+	]
 
-pdf_vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(), persist_directory=os.path.join(base_directory, "pdf_chroma_db"))
-pdf_vectorstore.persist()
+	gitbooks = [
+		'https://docs.echelon.io/'
+	]
 
-data_csv = []
-for document in csvs:
-	csv_loader = CSVLoader(file_path=document, encoding="utf-8")
-	data = csv_loader.load_and_split()
-	data_csv.extend(data)
+	for document in pdfs:
+		with open(document, "r") as f:
+			markdown_document = f.read()
 
-csv_vectorstore = Chroma.from_documents(documents=data_csv, embedding=OpenAIEmbeddings(), persist_directory=os.path.join(base_directory, "csv_chroma_db"))
-csv_vectorstore.persist()
+		headers_to_split_on = [
+			("##", "section"),
+			("###", "topic"),
+		]
 
-data_gitbook = []
-for document in gitbooks:
-	gitbook_loader = GitbookLoader(document, load_all_paths=True)
-	data = gitbook_loader.load_and_split()
-	data_gitbook.extend(data)
+		markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+		md_header_splits = markdown_splitter.split_text(markdown_document)    
+		splits.extend(md_header_splits)
 
-gitbook_vectorstore = Chroma.from_documents(documents=data_gitbook, embedding=OpenAIEmbeddings(), persist_directory=os.path.join(base_directory, "gitbook_chroma_db"))
-gitbook_vectorstore.persist()
+	pdf_vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(), persist_directory=os.path.join(base_directory, "pdf_chroma_db"))
+	pdf_vectorstore.persist()
+
+	data_csv = []
+	for document in csvs:
+		csv_loader = CSVLoader(file_path=document, encoding="utf-8")
+		data = csv_loader.load_and_split()
+		data_csv.extend(data)
+
+	csv_vectorstore = Chroma.from_documents(documents=data_csv, embedding=OpenAIEmbeddings(), persist_directory=os.path.join(base_directory, "csv_chroma_db"))
+	csv_vectorstore.persist()
+
+	data_gitbook = []
+	for document in gitbooks:
+		gitbook_loader = GitbookLoader(document, load_all_paths=True)
+		data = gitbook_loader.load_and_split()
+		data_gitbook.extend(data)
+
+	gitbook_vectorstore = Chroma.from_documents(documents=data_gitbook, embedding=OpenAIEmbeddings(), persist_directory=os.path.join(base_directory, "gitbook_chroma_db"))
+	gitbook_vectorstore.persist()
