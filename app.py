6ef2aad9f7b0308469f598f97e0a99b6d22626ac
s.saletti <s.saletti@qzrstudio.com>
2023-10-05 21:32:38 +0200

    push

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ from flask_cors import CORS
 from endpoints import query_bot_endpoint
 from utilities import initialize_tools  
 from embedding_generator import create_embeddings
+from dotenv import load_dotenv
+import os
 
-# these lines swap the stdlib sqlite3 lib with the pysqlite3 package
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+load_dotenv()
 
+DEPLOYMENT_ENV = os.environ.get('DEPLOYMENT_ENV', 'DEVELOPMENT')
+if DEPLOYMENT_ENV == 'PRODUCTION':
+	# these lines swap the stdlib sqlite3 lib with the pysqlite3 package
+	__import__('pysqlite3')
+	import sys
+	sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')	
 
 app = Flask(__name__)
 CORS(app)
